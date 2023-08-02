4b643ed5ad7bded526d4b2fd8dccfd2c51b4e842
sarah-die <saridi@alice.de>
2023-08-02 17:52:37 +0200

    Part b: first steps <until type narrowing>

---
+++ first-steps/multiplier.ts
@@ -0,0 +1,7 @@
+const multiplicator = (a: number, b: number, printText: string) => {
+    console.log(printText, a * b);
+}
+console.log("test");
+
+
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
