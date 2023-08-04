abf8757beb993e0bef8d785a973dc3549c8c3fbe
sarah-die <saridi@alice.de>
2023-08-04 16:37:59 +0200

    9.9: Patientor backen, step 2

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
+import cors from "cors";
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/43909#issuecomment-1168194740
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
